acb2abada2304f78b5198d70c9bc620cc921e00b
Viplav Kumar <thakurviplav88@gmail.com>
2025-07-27 13:26:09 +0530

    Creating App.js

---
+++ App.js
@@ -0,0 +1,8 @@
+const heading = React.createElement(
+        "h1",
+        {},
+        "Hello World! from Viplav Kumar"
+      );
+      const root = ReactDOM.createRoot(document.getElementById("root"));
+      root.render(heading);
+    //   ReactDOM.render(heading, document.getElementById("root"));
