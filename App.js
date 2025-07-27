c1f0d19b6fe436cc9746b25bf23a47fcda971bed
Viplav Kumar <thakurviplav88@gmail.com>
2025-07-27 15:13:42 +0530

    Making sibling and child elements

--- App.js
+++ App.js
@@ -1,9 +1,47 @@
-const heading = React.createElement(
-  "h1",
-  { id: "heading", className: "header" },
-  "Hello World! from Viplav Kumar"
-);
-console.log(heading);
+/**
+ *
+ * <div id="grandParent">
+ *      <div id="parent1">
+ *          <h3 id="child11">I am 1st H3 Tag of Parent-1</h3>
+ *          <h3 id="child12">I am 2nd H3 Tag of Parent-1</h3>
+ *      </div>
+ *      <div id="parent2">
+ *          <h3 id="child21">I am 1st H3 Tag of Parent-2</h3>
+ *          <h3 id="child22">I am 2nd H3 Tag of Parent-2</h3>
+ *      </div>
+ * </div>
+ *
+ *
+ */
+
+const myDom = React.createElement("div", { id: "grandParent" }, [
+  React.createElement("div", { id: "parent1", key: 1 }, [
+    React.createElement(
+      "h3",
+      { id: "child11", key: 1 },
+      "I am 1st H3 Tag  of Parent-1"
+    ),
+    React.createElement(
+      "h3",
+      { id: "child12", key: 2 },
+      "I am 2nd H3 Tag of Parent-1"
+    ),
+  ]),
+  React.createElement("div", { id: "parent2", key: 2 }, [
+    React.createElement(
+      "h3",
+      { id: "child21", key: 1 },
+      "I am 1st H3 Tag of Parent-2"
+    ),
+    React.createElement(
+      "h3",
+      { id: "child22", key: 2 },
+      "I am 2nd H3 Tag of Parent-2"
+    ),
+  ]),
+]);
+
+console.log(myDom);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(myDom);
 //   ReactDOM.render(heading, document.getElementById("root"));
